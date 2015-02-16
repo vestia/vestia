afe48065630dfc4fa956c8711296cf9a5cd88dc2
gardnersmitha <austingardnersmith@gmail.com>
2015-02-16 04:02:18 +0000

    adding a homes controller to test basic functionality - @/homes now works

---
+++ app/Http/Controllers/HomesController.php
@@ -0,0 +1,85 @@
+<?php namespace Vestia\Http\Controllers;
+
+use Vestia\Http\Requests;
+use Vestia\Http\Controllers\Controller;
+use Vestia\Home;
+use Illuminate\Http\Request;
+
+class HomesController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 * /homes/
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return Home::all(); 
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
